a3fb958a5b1ce5593ae75f6c279cdd5643f6939c
Thibault Forret <forret.thibault@gmail.com>
2024-05-31 08:10:09 +0200

    first page done

--- app/src/main/java/com/example/minijeucalculmental/MainActivity.java
+++ app/src/main/java/com/example/minijeucalculmental/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.minijeucalculmental;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonJouer;
+    private Button buttonMeilleursScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonJouer = findViewById(R.id.bouton_jouer);
+        buttonMeilleursScore = findViewById(R.id.meilleurs_score);
+
+        buttonJouer.setOnClickListener(view -> {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
         });
+
+        buttonMeilleursScore.setOnClickListener(view -> {
+            Intent intent = new Intent(this, HighscoreActivity.class);
+            startActivity(intent);
+        });
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
     }
 }
