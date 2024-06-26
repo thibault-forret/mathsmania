718ef8d2eb995fb67b9444df5dcbcb66d6c8353f
Thibault Forret <forret.thibault@gmail.com>
2024-06-26 20:13:40 +0200

    final app

--- app/src/main/java/com/example/minijeucalculmental/GameActivity.java
+++ app/src/main/java/com/example/minijeucalculmental/GameActivity.java
@@ -1,9 +1,12 @@
 package com.example.minijeucalculmental;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
+    private LinearLayout gameLayout;
     private Button replyButton;
     private EditText replyInput;
     private TextView numberOfScore;
@@ -29,6 +33,9 @@ public class GameActivity extends AppCompatActivity {
     private int lives;
     private int currentAnswer;
     private int difficulty;
+    private int gameMode;
+    private int gameModeLives;
+    private int userChooseLives;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +59,8 @@ public class GameActivity extends AppCompatActivity {
         };
         getOnBackPressedDispatcher().addCallback(   this, callback);
 
+        gameLayout = findViewById(R.id.GameLayout);
+
         replyButton = findViewById(R.id.reply_button);
         replyInput = findViewById(R.id.input_reply);
 
@@ -59,44 +68,97 @@ public class GameActivity extends AppCompatActivity {
         numberOfScore = findViewById(R.id.number_of_score);
         numberOfLife = findViewById(R.id.number_of_life);
 
+
+        // Récupère le score envoyé par l'activité précédente
+        Intent intent = getIntent();
+        if (intent != null) {
+            if(intent.hasExtra("DIFFICULTY"))
+                difficulty = intent.getIntExtra("DIFFICULTY",1);
+            if(intent.hasExtra("GAMEMODE"))
+                gameMode = intent.getIntExtra("GAMEMODE", 1);
+            if(intent.hasExtra("GAMEMODE_LIVES"))
+                gameModeLives = intent.getIntExtra("GAMEMODE_LIVES", 1);
+            if(intent.hasExtra("LIVES"))
+                userChooseLives = intent.getIntExtra("LIVES", 5);
+        }
+        // Récupérer depuis l'ancienne vue (pas eu le temps de le faire)
         difficulty = 1;
+        // gameMode = 1;
+        // gameModeLives = 1;
 
         initializeGame();
 
         replyButton.setOnClickListener(view -> checkAnswer());
     }
 
+//    private void initializeLives() {
+//        switch (gameModeLives) {
+//            case 1: // Sans choix du nombre de vie
+//                lives = 5;
+//            case 2: // Choix du nombre de vie
+//                lives = userChooseLives;
+//            case 3: // Mode one shot
+//                lives = 1;
+//            default :
+//                lives = 5;
+//        }
+//    }
+
     private void initializeGame() {
         score = 0;
         lives = 5;
+
+        // initializeLives(); // Fonctionnalité pour choisir le nombre de vie, mais pas eu le temps de finir
+
         numberOfScore.setText(String.valueOf(score));
         numberOfLife.setText(String.valueOf(lives));
 
         generateQuestion();
     }
 
+    private int getDifficultyAndColorWithScore() {
+        int color;
+
+        if(score < 5) {
+            difficulty = 1;
+            color = getColor(R.color.difficulty_1);
+        }
+        else if(score < 10) {
+            difficulty = 2;
+            color = getColor(R.color.difficulty_2);
+        }
+        else if(score < 15) {
+            difficulty = 3;
+            color = getColor(R.color.difficulty_3);
+        }
+        else if(score < 20) {
+            difficulty = 4;
+            color = getColor(R.color.difficulty_4);
+        }
+        else if (score < 25) {
+            difficulty = 5;
+            color = getColor(R.color.difficulty_5);
+        }
+        else {
+            difficulty = 6;
+            color = getColor(R.color.difficulty_6);
+        }
+        return color;
+    }
+
     private void generateQuestion() {
         Random random = new Random();
         int a, b;
         String operator = "";
+        int color;
 
-        if(score < 5)
-            difficulty = 1;
-        else
-            if(score < 10)
-                difficulty = 2;
-            else
-                if(score < 15)
-                    difficulty = 3;
-                else
-                    if(score < 20)
-                        difficulty = 4;
-                    else
-                        difficulty = 5;
+        color = getDifficultyAndColorWithScore();
+        gameLayout.setBackgroundColor(color);
+        replyButton.setTextColor(color);
 
         switch (difficulty) {
             case 1:
-                // Niveau 1 : De 1 à 10
+                // Niveau 1 : De 1 à 10 - Sans multiplication
                 a = random.nextInt(10) + 1;
                 b = random.nextInt(10) + 1;
                 break;
@@ -111,24 +173,29 @@ public class GameActivity extends AppCompatActivity {
                 b = random.nextInt(901) + 100;
                 break;
             case 4:
-                // Niveau 4 : De 10 à 100 - Avec multiplication
+                // Niveau 4 : De 1 à 10 - Avec multiplication
+                a = random.nextInt(10) + 1;
+                b = random.nextInt(10) + 1;
+                break;
+            case 5:
+                // Niveau 5 : De 10 à 100 - Avec multiplication
                 a = random.nextInt(91) + 10;
                 b = random.nextInt(91) + 10;
                 break;
-            case 5:
-                // Niveau 5 : De 100 à 1000 - Avec multiplication
+            case 6:
+                // Niveau 6 : De 100 à 1000 - Avec multiplication
                 a = random.nextInt(901) + 100;
                 b = random.nextInt(901) + 100;
                 break;
             default:
-                // Niveau 1 : De 1 à 10
+                // Niveau 1 : De 1 à 10 - Sans multiplication
                 a = random.nextInt(10) + 1;
                 b = random.nextInt(10) + 1;
         }
 
         int operatorType;
 
-        if(difficulty == 2 || difficulty == 3)
+        if(difficulty == 1 || difficulty == 2 || difficulty == 3)
             operatorType = random.nextInt(2);
         else
             // L'opérateur devient la multiplication pour tous les calculs
